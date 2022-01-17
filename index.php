427c197479e133a7807953617d3ae660fc91acb4
Faisal Baig <mirzaf317@gmail.com>
2022-01-18 00:59:34 +0530

    added new files

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Isfan Inc.</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Castoro&family=Courgette&family=Merienda&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="header">
+        <p id="italic">Isfan
+        <p id="normal">.com</p>
+        </p>
+    </div>
+    <!-- Image Slider Starts Here -->
+    <div class="slider">
+        <img src="https://source.unsplash.com/random/2200x500/?computer" alt="Image Will be shown here" class="img">
+    </div>
+    <div class="br"></div>
+    <!-- Image Slider Ends Here -->
+    <div class="gallery">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+    </div>
+    
+</body>
+
+</html>
