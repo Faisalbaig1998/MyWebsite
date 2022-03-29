b590c59284ab0f2233322e2d7b31c2b068cac777
Faisalbaig1998 <mirzaf317@gmail.com>
2022-03-30 01:37:14 +0530

    Nothing

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,11 @@
 <footer>
-    <h1>I'm a footer</h1>
+    <div class="main-menu">
+        <ul>
+            <li>New Arrivals</li>
+            <li>About</li>
+        </ul>
+    </div>
+    <div class="policy"></div>
+    <div class="address"></div>
+
 </footer>
